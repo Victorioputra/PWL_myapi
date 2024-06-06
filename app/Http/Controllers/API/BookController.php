812d606829db9778ab05632280f3105f8a0c27a8
nedd18 <benedbened18@gmail.com.>
2024-06-07 01:07:55 +0700

    api book initial 3

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -18,64 +18,160 @@ use OpenApi\Annotations as OA;
 
 class BookController extends Controller
 {
-
-    /** 
+    /**
      * @OA\Get(
      *     path="/api/book",
      *     tags={"Book"},
-     *     summary="Display a listing of the items",
+     *     summary="Display a listing of items",
      *     operationId="index",
      *     @OA\Response(
      *         response=200,
      *         description="successful",
      *         @OA\JsonContent()
-     *     )
+     *     ),
+     *     @OA\Parameter(
+     *         name="_page",
+     *         in="query",
+     *         description="current page",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=1
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="_limit",
+     *         in="query",
+     *         description="max item in a page",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=10
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="_search",
+     *         in="query",
+     *         description="word to search",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="_publisher",
+     *         in="query",
+     *         description="search by publisher like name",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string",
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="_sort_by",
+     *         in="query",
+     *         description="word to search",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string",
+     *             example="latest"
+     *         )
+     *     ),
      * )
      */
-    public function index()
+
+    public function index(Request $request)
     {
-        return Book::get();
+        try {
+            $data['filter']       = $request->all();
+            $page                 = $data['filter']['_page']  = (@$data['filter']['_page'] ? intval($data['filter']['_page']) : 1);
+            $limit                = $data['filter']['_limit'] = (@$data['filter']['_limit'] ? intval($data['filter']['_limit']) : 1000);
+            $offset               = ($page?($page-1)*$limit:0);
+            $data['products']     = Book::whereRaw('1 = 1');
+            
+            if($request->get('_search')){
+                $data['products'] = $data['products']->whereRaw('(LOWER(title) LIKE "%'.strtolower($request->get('_search')).'%" OR LOWER(author) LIKE "%'.strtolower($request->get('_search')).'%")');
+            }
+            if($request->get('_publisher')){
+                $data['products'] = $data['products']->whereRaw('LOWER(publisher) = "'.strtolower($request->get('_publisher')).'"');
+            }
+            if($request->get('_sort_by')){
+            switch ($request->get('_sort_by')) {
+                default:
+                case 'latest_publication':
+                $data['products'] = $data['products']->orderBy('publication_year','DESC');
+                break;
+                case 'latest_added':
+                $data['products'] = $data['products']->orderBy('created_at','DESC');
+                break;
+                case 'title_asc':
+                $data['products'] = $data['products']->orderBy('title','ASC');
+                break;
+                case 'title_desc':
+                $data['products'] = $data['products']->orderBy('title','DESC');
+                break;
+                case 'price_asc':
+                $data['products'] = $data['products']->orderBy('price','ASC');
+                break;
+                case 'price_desc':
+                $data['products'] = $data['products']->orderBy('price','DESC');
+                break;
+            }
+            }
+            $data['products_count_total']   = $data['products']->count();
+            $data['products']               = ($limit==0 && $offset==0)?$data['products']:$data['products']->limit($limit)->offset($offset);
+            // $data['products_raw_sql']       = $data['products']->toSql();
+            $data['products']               = $data['products']->get();
+            $data['products_count_start']   = ($data['products_count_total'] == 0 ? 0 : (($page-1)*$limit)+1);
+            $data['products_count_end']     = ($data['products_count_total'] == 0 ? 0 : (($page-1)*$limit)+sizeof($data['products']));
+           return response()->json($data, 200);
+
+        } catch(\Exception $exception) {
+            throw new HttpException(400, "Invalid data : {$exception->getMessage()}");
+        }
     }
 
-
     /**
-     * @OA\Post(
-     *     path="/api/book",
-     *     tags={"Book"},
-     *     summary="Store a newly created item",
-     *     operationId="store",
-     *     @OA\Response(
-     *         response=400,
-     *         description="Invalid input",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\Response(
-     *         response=201,
-     *         description="Successful",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\RequestBody(
-     *         required=true,
-     *         description="Request body description",
-     *         @OA\JsonContent(
-     *             ref="#/components/schemas/Book",
-     *             example={"title": "Eating Clean", "author": "Inge Tumiwa-Bachrens", "publisher": "Kawan Pustaka", "publication_year": "2016",
-     *                      "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1482170055i/33511107.jpg",
-     *                      "description": "Menjadi sehat adalah impian semua orang. Makanan yang selama ini kita pikir sehat ternyata belum tentu 'sehat' bagi tubuh kita.",
-     *                      "price": 85000}
-     *         ),
-     *     ),
-     *      security={{"passport_token_ready":{},"passport":{}}}
-     * )
-     */
+    * @OA\Post(
+    *     path="/api/book",
+    *     tags={"Book"},
+    *     summary="Store a newly created item",
+    *     operationId="store",
+    *     @OA\Response(
+    *         response=400,
+    *         description="Invalid input",
+    *         @OA\JsonContent()
+    *     ),
+    *     @OA\Response(
+    *         response=201,
+    *         description="Successful",
+    *         @OA\JsonContent()
+    *     ),
+    *     @OA\RequestBody(
+    *         required=true,
+    *         description="Request body description",
+    *         @OA\JsonContent(
+    *             ref="#/components/schemas/Book",
+    *             example={"title": "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!", "author": "Robert T. Kiyosaki dengan Sharon Lechter", "publisher": "Plata Publishing", "publication_year": "2011", 
+    *                      "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/book/1482170055i/33511107.jpg", 
+    *                      "description": "Rich Dad Poor Dad adalah buku yang mengubah cara orang memandang uang dan investasi. Robert T. Kiyosaki menceritakan pengalamannya dibesarkan oleh dua 
+    *                        ayah—ayah kandungnya yang miskin dan ayah angkatnya yang kaya—dan pelajaran yang dia pelajari dari keduanya. Buku ini menekankan pentingnya literasi finansial, investasi, dan pengelolaan keuangan yang bijak. Kiyosaki mengajak pembaca untuk berpikir kritis tentang bagaimana mereka dapat mencapai kebebasan finansial melalui pendidikan dan pengambilan keputusan yang tepat.", 
+    *                      "price": 204000}
+    *         ),
+    *     ),
+    *     security={{"passport_token_ready":{},"passport":{}}}
+    * )
+    */
 
     public function store(Request $request)
     {
-        try{
+        try {
             $validator = Validator::make($request->all(), [
                 'title'  => 'required|unique:books',
                 'author'  => 'required|max:100',
-            ]);
+            ]); 
             if ($validator->fails()) {
                 throw new HttpException(400, $validator->messages()->first());
             }
@@ -84,45 +180,46 @@ class BookController extends Controller
             return $book;
 
         } catch(\Exception $exception) {
-            throw new HttpException(400, "Invalid Data : {$exception->getMessage()}");
+            throw new HttpException(400, "Invalid data : {$exception->getMessage()}");
         }
     }
 
 
     /**
-     * @OA\Get(
-     *     path="/api/book/{id}",
-     *     tags={"Book"},
-     *     summary="Display the specified item",
-     *     operationId="show",
-     *     @OA\Response(
-     *         response=404,
-     *         description="Item not found",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\Response(
-     *         response=400,
-     *         description="Invalid input",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful",
-     *         @OA\JsonContent()
-     *     ),
-     *     @OA\Parameter(
-     *         name="id",
-     *         in="path",
-     *         description="ID of item that needs to be displayed",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="integer",
-     *             format="int64"
-     *         )
-     *     ),
-     * )
-     */
-
+    * @OA\Get(
+    *     path="/api/book/{id}",
+    *     tags={"Book"},
+    *     summary="Display the specified item",
+    *     operationId="show",
+    *     @OA\Response(
+    *         response=404,
+    *         description="Item not found",
+    *         @OA\JsonContent()
+    *     ),
+    *     @OA\Response(
+    *         response=400,
+    *         description="Invalid input",
+    *         @OA\JsonContent()
+    *     ),
+    *     @OA\Response(
+    *         response=200,
+    *         description="Successful",
+    *         @OA\JsonContent()
+    *     ),
+    *     @OA\Parameter(
+    *         name="id",
+    *         in="path",
+    *         description="ID of item that needs to be displayed",
+    *         required=true,
+    *         @OA\Schema(
+    *             type="integer",
+    *             format="int64"
+    *         )
+    *     ),
+    * )
+    */
+    
+    
     public function show($id)
     {
         $book = Book::find($id);
@@ -145,12 +242,12 @@ class BookController extends Controller
      *     ),
      *     @OA\Response(
      *         response=400,
-     *         description="Invalid input",
+     *         description="invalid input",
      *         @OA\JsonContent()
      *     ),
      *     @OA\Response(
      *         response=200,
-     *         description="Successful",
+     *         description="successful",
      *         @OA\JsonContent()
      *     ),
      *     @OA\Parameter(
@@ -168,28 +265,28 @@ class BookController extends Controller
      *         description="Request body description",
      *         @OA\JsonContent(
      *             ref="#/components/schemas/Book",
-     *             example={"title": "Eating Clean", "author": "Inge Tumiwa-Bachrens", "publisher": "Kawan Pustaka", "publication_year": "2016",
-     *                      "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1482170055i/33511107.jpg",
-     *                      "description": "Menjadi sehat adalah impian semua orang. Makanan yang selama ini kita pikir sehat ternyata belum tentu 'sehat' bagi tubuh kita.",
-     *                      "price": 85000}
+     *             example={"title": "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!", "author": "Robert T. Kiyosaki dengan Sharon Lechter", "publisher": "Plata Publishing", "publication_year": "2011", 
+    *                      "cover": "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/book/1482170055i/33511107.jpg", 
+    *                      "description": "Rich Dad Poor Dad adalah buku yang mengubah cara orang memandang uang dan investasi. Robert T. Kiyosaki menceritakan pengalamannya dibesarkan oleh dua 
+    *                        ayah—ayah kandungnya yang miskin dan ayah angkatnya yang kaya—dan pelajaran yang dia pelajari dari keduanya. Buku ini menekankan pentingnya literasi finansial, investasi, dan pengelolaan keuangan yang bijak. Kiyosaki mengajak pembaca untuk berpikir kritis tentang bagaimana mereka dapat mencapai kebebasan finansial melalui pendidikan dan pengambilan keputusan yang tepat.", 
+    *                      "price": 204000}
      *         ),
      *     ),
-     *      security={{"passport_token_ready":{},"passport":{}}}
+     *     security={{"passport_token_ready":{},"passport":{}}}
      * )
      */
-
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
         $book = Book::find($id);
         if(!$book){
             throw new HttpException(404, 'Item not found');
         }
 
-        try{
+        try {
             $validator = Validator::make($request->all(), [
                 'title'  => 'required|unique:books',
                 'author'  => 'required|max:100',
-            ]);
+            ]); 
             if ($validator->fails()) {
                 throw new HttpException(400, $validator->messages()->first());
             }
@@ -197,10 +294,10 @@ class BookController extends Controller
            return response()->json(array('message'=>'Updated successfully'), 200);
 
         } catch(\Exception $exception) {
-            throw new HttpException(400, "Invalid Data : {$exception->getMessage()}");
+           throw new HttpException(400, "Invalid data : {$exception->getMessage()}");
         }
     }
-
+    
     /**
      * @OA\Delete(
      *     path="/api/book/{id}",
@@ -232,11 +329,10 @@ class BookController extends Controller
      *             format="int64"
      *         )
      *     ),
-     *      security={{"passport_token_ready":{},"passport":{}}}
+     *     security={{"passport_token_ready":{},"passport":{}}}
      * )
      */
-    
-    public function destroy(string $id)
+    public function destroy($id)
     {
         $book = Book::find($id);
         if(!$book){
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app-public')
+@section('title', 'Home')
+@section('content')
+    <div class="site-wrapper-reveal">
+        <div class="hero-box-area">
+            <div class="container">
+                <div class="row ">
+                    <div class="col-lg-12">
+                        <!-- Hero Slider Area Start -->
+                        <div class="hero-area" id="product-preview">
+                        </div>
+                        <!-- Hero Slider Area End -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="about-us-area section-space--ptb_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="about-us-content_6 text-center">
+                            <h2>B.N.B.S&nbsp;&nbsp;Store</h2>
+                            <p>
+                                <small>
+                                Welcome to our Bed and Bath Store! We believe in transforming your home into a sanctuary of comfort and style. Whether you're seeking the latest bath and bedding essentials, timeless classics, or unique hidden treasures,
+                                our thoughtfully curated collection has something for everyone. Our passionate and knowledgeable staff is dedicated to helping you find the perfect items to suit your taste and needs. From luxurious sheets and cozy blankets
+                                to elegant bath towels and unique decor pieces, we offer products that blend quality with design. Explore our cozy and welcoming environment, where you can take your time to browse and discover. Visit us today and experience 
+                                the joy of finding exceptional products that enhance your daily life. Let us help you turn your living spaces into the ultimate retreats of relaxation and style. &#10084;
+                                </small>
+                            </p>
+                            <p class="mt-5">Find your window to the world! Or, even, 
+                                <span class="text-color-primary">unlock hidden worlds, one page at a time!</span> 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Banner Video Area Start -->
+         <div class="banner-video-area overflow-hidden">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="banner-video-box">
+                            <img src="https://www.magd.ox.ac.uk/wp-content/uploads/2022/07/3027-052-2000x1000.jpg" alt="">
+                            <div class="video-icon">
+                                <a href="https://youtu.be/Na5KPnx0uS8?si=PPoJfum8xG-Jt8h9" class="popup-youtube"><i class="linear-ic-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Banner Video Area End -->
+
+        <!-- Our Brand Area Start -->
+        <div class="our-brand-area section-space--pb_90">
+            <div class="container">
+                <div class="brand-slider-active">
+                    @php
+                        $partner_count = 8;
+                    @endphp
+                    @for($i=1;$i<=$partner_count;$i++)
+                        <div class="col-lg-12">
+                            <div class="single-brand-item">
+                                <a href="#"><img src="assets/images/brand/partnerb{{$i}}.jpg" class="img-fluid" alt="Partner Images"></a>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+            </div>
+        </div>
+        <!-- Our Brand Area End -->
+
+        <!-- Our Member Area Start -->
+        <div class="our-member-area section-space--ptb_120">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="member--box">
+                            <div class="row align-items-center">
+                                <div class="col-lg-5 col-md-4">
+                                    <div class="section-title small-mb__40 tablet-mb__40">
+                                        <h4 class="section-title">Join the community!</h4>
+                                        <p>Become one of the member and get discount 50% off</p>
+                                    </div>
+                                </div>
+                                <div class="col-lg-7 col-md-8">
+                                    <div class="member-wrap">
+                                        <form action="#" class="member--two">
+                                            <input class="input-box" type="text" placeholder="Your email address">
+                                            <button class="submit-btn"><i class="icon-arrow-right"></i></button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Our Member Area End -->
+
+    </div>
+@endsection
+@section('addition_css')
+@endsection
+@section('addition_script')
+    <script src="{{asset('pages/js/home.js')}}"></script>
+@endsection
